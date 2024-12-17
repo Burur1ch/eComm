0332c4e94a3e83d4ef710a6994bd5977f442a2e9
Ilya <Irresistible0@mail.ru>
2024-12-17 12:03:19 +0300

    preWork

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { Store } from 'redux'
+import React from 'react'
+import { Provider } from 'react-redux'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
